59c7b622b2d91c449abb6378cd59a22673ca9696
olaals <olaalstad96@gmail.com>
2020-06-22 10:27:45 +0200

    Toggle color of box with click

---
+++ interaction/toggle_color.js
@@ -0,0 +1,18 @@
+console.log("added succ sfsdfds")
+
+
+box_span = document.getElementById('box')
+
+var toggle = 0;
+
+box_span.addEventListener('click', function (evt) {
+    if(toggle == 0){
+        box_span.setAttribute('material', 'color', 'green');
+        toggle = 1;
+    }
+    else{
+        box_span.setAttribute('material', 'color', 'red');
+        toggle = 0;
+    }
+  });
+
